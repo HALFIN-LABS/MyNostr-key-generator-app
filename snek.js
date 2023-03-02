593bdadba2ad267b1e8cb5d36646a992e87fc005
ImTiaan <t@imtiaan.com>
2023-03-02 00:12:45 +0000

    who knows

--- snek.js
+++ snek.js
@@ -1,6 +1,22 @@
 function snek() {
+    // Get input values from form
+    var prefix = document.getElementById("prefixRadio").value;
+    var suffix = document.getElementById("suffixRadio").value;
+
     // Get the snackbar DIV
     var x = document.getElementById("snackbar");
+
+    if (prefix || suffix) {
+      // If at least one input value is not empty, display personalized message
+      if (prefix) {
+        snackbar.innerText = "Your Nostr keys with the " + prefix + " suffix is being generated.";
+      } else if (suffix) {
+        snackbar.innerText = "Your Nostr keys with the " + suffix + " prefix is being generated.";
+      }
+     else {
+      // If both input values are empty, display generic message
+      snackbar.innerText = "Your Nostr keys are being generated.";
+    }
   
     // Add the "show" class to DIV
     x.className = "show";
